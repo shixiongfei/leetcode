5edf1390a4bff8eeee945de3c1ce26fdf8f3018b
shixiongfei <xiongfei.shi@icloud.com>
2022-01-18 20:54:36 +0800

    两整数之和

---
+++ sum_of_two_integers.erl
@@ -0,0 +1,15 @@
+-module(sum_of_two_integers).
+-export([get_sum/2]).
+
+-spec get_sum(A :: integer(), B :: integer()) -> integer().
+get_sum(A, B) when B == 0 ->
+  if
+    %% 正数直接返回
+    A < 16#80000000 -> A;
+    %% 负数需要返回补码
+    true -> -((bnot(A) band 16#ffffffff) + 1)
+  end;
+get_sum(A, B) ->
+  Sum =(A bxor B) band 16#ffffffff,
+  Carry = (A band B bsl 1) band 16#ffffffff,
+  get_sum(Sum, Carry).
