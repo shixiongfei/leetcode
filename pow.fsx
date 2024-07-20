a07ed47a946bce7115f2e31e3602276045b82b7c
shixiongfei <xiongfei.shi@icloud.com>
2024-07-20 14:56:18 +0800

    Pow(x, n) match version

--- pow.fsx
+++ pow.fsx
@@ -3,9 +3,10 @@
 
 let myPow (x: float) (n: int) =
     let rec myPowHelper accum x n =
-        if n = 0 then accum
-        elif (n % 2) = 0 then myPowHelper accum (x * x) (n / 2)
-        elif n < 0 then myPowHelper 1.0 (1.0 / x) -n
-        else myPowHelper (accum * x) x (n - 1)
+        match n with
+        | _ when n = 0 -> accum
+        | _ when (n % 2) = 0 -> myPowHelper accum (x * x) (n / 2)
+        | _ when n < 0 -> myPowHelper 1.0 (1.0 / x) -n
+        | _ -> myPowHelper (accum * x) x (n - 1)
 
     myPowHelper 1.0 x n
